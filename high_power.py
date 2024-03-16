e459b8dfef44a6e472234ad635277536a3a78e3f
Syo <155808885+syo33331@users.noreply.github.com>
2024-03-16 23:18:53 +0900

    Change the number of members you want to acquire

--- high_power.py
+++ high_power.py
@@ -5,6 +5,7 @@ import random
 import uuid
 import pyfiglet
 from colorama import init, Fore
+import time
 
 init()
 
@@ -12,11 +13,12 @@ text = "Yay! Spammer"
 font = "slant"
 
 ascii_art = pyfiglet.figlet_format(text, font=font)
+
 print(Fore.MAGENTA + ascii_art + Fore.RESET)
 
 async def fetch_group_members(session, group_id, tokens):
-    token = random.choice(tokens)
-    url = f"https://api.yay.space/v2/groups/{group_id}/members"
+    token = random.choice(tokens)  
+    url = f"https://api.yay.space/v2/groups/{group_id}/members?number=300"
     headers = {"Authorization": f"Bearer {token}"}
     async with session.get(url, headers=headers) as response:
         assert response.status == 200
@@ -24,12 +26,12 @@ async def fetch_group_members(session, group_id, tokens):
         return [{"id": member["user"]["id"], "nickname": member["user"]["nickname"]} for member in data["group_users"]]
 
 async def send_message(session, group_id, member, base_message, tokens):
-    token = random.choice(tokens)
+    token = random.choice(tokens)  
     url = "https://yay.space/api/posts"
     headers = {"Authorization": f"Bearer {token}"}
     text = f"@{member['nickname']} {base_message}" if member else base_message
     message_tags = json.dumps([{"type": "user", "user_id": member['id'], "offset": 0, "length": len(member['nickname']) + 1}]) if member else "[]"
-
+    
     post_data = {
         "post_type": "text",
         "text": text,
@@ -39,11 +41,11 @@ async def send_message(session, group_id, member, base_message, tokens):
         "group_id": group_id,
         "uuid": str(uuid.uuid4())
     }
-
+    
     async with session.post(url, headers=headers, json=post_data) as response:
         response_text = await response.text()
         print(f"Status Code: {response.status}, Response: {response_text}")
-        assert response.status == 201
+        assert response.status == 201  
 
 async def main():
     group_id = input("グループIDを入力してください: ")
@@ -53,14 +55,11 @@ async def main():
 
     with open("token.txt", "r") as file:
         tokens = [line.strip() for line in file.readlines() if line.strip()]
-
+    
     async with aiohttp.ClientSession() as session:
         members = await fetch_group_members(session, group_id, tokens)
-        for i in range(num_messages):
-            if i > 0 and i % (10 // 2) == 0: 
-                members = await fetch_group_members(session, group_id, tokens)
+        for _ in range(num_messages):
             member = random.choice(members) if mention_random_member and members else None
             await send_message(session, group_id, member, base_message, tokens)
-            await asyncio.sleep(2)  
 
 asyncio.run(main())
